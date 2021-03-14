29c039c41b3e0a04409980bada3c0b391d76cbd7
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2021-03-14 16:46:09 +0300

    Add beginner path

---
+++ beginner-path/getting-started-with-javascript/primer/ex.js
@@ -0,0 +1,27 @@
+// TODO: define addFavoriteBook(..) function
+
+function addFavoriteBook(bookName) {
+    if(!bookName.includes('Great')) {
+        favoriteBooks.push(bookName)
+    } 
+}
+// TODO: define printFavoriteBooks() function
+
+var favoriteBooks = [];
+
+addFavoriteBook("A Song of Ice and Fire");
+addFavoriteBook("The Great Gatsby");
+addFavoriteBook("Crime & Punishment");
+addFavoriteBook("Great Expectations");
+addFavoriteBook("You Don't Know JS");
+
+// TODO: print out favorite books
+
+function printFavoriteBooks() {
+    console.log(`Favorite books: ${favoriteBooks.length}`)
+    for ( let bookName of favoriteBooks) {
+        console.log(bookName)
+    }
+}
+
+printFavoriteBooks()
