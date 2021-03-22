57ebce9800b621499e9c7b006e55eb6ed4f5d402
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2021-03-22 22:46:20 +0300

    Add coercion exercise

--- professional-path/deep-js-foundations-v2/types-exercises/coercion/ex.js
+++ professional-path/deep-js-foundations-v2/types-exercises/coercion/ex.js
@@ -1,6 +1,37 @@
 // TODO: write the validation functions
 
+function isValidName(name){
+    if (typeof name == "string" && 
+    name.trim().length >= 3) 
+    {
+        return true
+    }
+    return false
+}
 
+function hoursAttended(attended, length) {
+    if(typeof attended == "string" &&
+    attended.trim() != "")
+    {
+        attended = Number(attended)
+    } 
+    if (typeof length == "string" && 
+    length.trim() != "")
+    {
+        length = Number(length)
+    }
+    if(typeof attended == "number" &&
+    typeof length == "number" &&
+    attended >= 0 &&
+    length >= 0 &&
+    attended <= length &&
+    Number.isInteger(attended) &&
+    Number.isInteger(length))
+    {
+        return true
+    }
+    return false 
+}
 
 // tests:
 console.log(isValidName("Frank") === true);
