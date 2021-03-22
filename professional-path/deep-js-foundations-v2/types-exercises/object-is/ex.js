cec51a14d420d16a262a6fe5119e6a64d8284fb1
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2021-03-22 20:43:11 +0300

    Add object.is solution

--- professional-path/deep-js-foundations-v2/types-exercises/object-is/ex.js
+++ professional-path/deep-js-foundations-v2/types-exercises/object-is/ex.js
@@ -1,6 +1,27 @@
 // TODO: define polyfill for `Object.is(..)`
 
+if (!Object.is || true) {
+	Object.is = function ObjectIs(x, y) {
+        var xNegZero = isItNegZero(x)
+        var yNegZero = isItNegZero(y)
 
+        if(isItNegZero(x) || isItNegZero(y)){
+            return xNegZero && yNegZero
+        } else if (isItNaN(x) && isItNaN(y)){
+            return true
+        } else {
+            return x === y
+        }
+
+        function isItNegZero(v){
+            return v == 0 && (1/v) === -Infinity
+        }
+
+        function isItNaN(v){
+            return v !== v
+        }
+    }
+}
 
 // tests:
 console.log(Object.is(42,42) === true);
