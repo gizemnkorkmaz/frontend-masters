4d2026d5abb01a7a5d1f16ac0d9aa7ebdbf80f46
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2021-03-25 10:48:28 +0300

    Add hoisting exercise

--- professional-path/deep-js-foundations-v2/scope-exercises/hoisting/ex.js
+++ professional-path/deep-js-foundations-v2/scope-exercises/hoisting/ex.js
@@ -1,44 +1,3 @@
-function getStudentFromId(studentId) {
-	return studentRecords.find(function matchId(record){
-		return (record.id == studentId);
-	});
-}
-
-function printRecords(recordIds) {
-	var records = recordIds.map(getStudentFromId);
-
-	records.sort(function sortByNameAsc(record1,record2){
-		if (record1.name < record2.name) return -1;
-		else if (record1.name > record2.name) return 1;
-		else return 0;
-	});
-
-	records.forEach(function printRecord(record){
-		console.log(`${record.name} (${record.id}): ${record.paid ? "Paid" : "Not Paid"}`);
-	});
-}
-
-function paidStudentsToEnroll() {
-	var recordsToEnroll = studentRecords.filter(function needToEnroll(record){
-		return (record.paid && !currentEnrollment.includes(record.id));
-	});
-
-	var idsToEnroll = recordsToEnroll.map(function getStudentId(record){
-		return record.id;
-	});
-
-	return [ ...currentEnrollment, ...idsToEnroll ];
-}
-
-function remindUnpaid(recordIds) {
-	var unpaidIds = recordIds.filter(function notYetPaid(studentId){
-		var record = getStudentFromId(studentId);
-		return !record.paid;
-	});
-
-	printRecords(unpaidIds);
-}
-
 
 // ********************************
 
@@ -62,6 +21,60 @@ currentEnrollment = paidStudentsToEnroll();
 printRecords(currentEnrollment);
 console.log("----");
 remindUnpaid(currentEnrollment);
+
+function getStudentFromId(studentId) {
+	return studentRecords.find(matchId);
+
+	function matchId(record){
+		return (record.id == studentId);
+	}
+}
+
+function printRecords(recordIds) {
+	var records = recordIds.map(getStudentFromId);
+
+	records.sort(sortByNameAsc);
+
+	records.forEach(printRecord);
+}
+
+function sortByNameAsc(record1,record2){
+	if (record1.name < record2.name) return -1;
+	else if (record1.name > record2.name) return 1;
+	else return 0;
+}
+
+function printRecord(record){
+	console.log(`${record.name} (${record.id}): ${record.paid ? "Paid" : "Not Paid"}`);
+}
+
+function paidStudentsToEnroll() {
+	var recordsToEnroll = studentRecords.filter(needToEnroll);
+
+	var idsToEnroll = recordsToEnroll.map(getStudentId);
+
+	return [ ...currentEnrollment, ...idsToEnroll ];
+}
+
+function getStudentId(record){
+	return record.id;
+}
+
+function needToEnroll(record){
+	return (record.paid && !currentEnrollment.includes(record.id));
+}
+
+function remindUnpaid(recordIds) {
+	var unpaidIds = recordIds.filter(notYetPaid);
+
+	printRecords(unpaidIds);
+}
+
+function notYetPaid(studentId){
+	var record = getStudentFromId(studentId);
+	return !record.paid;
+}
+
 
 /*
 	Bob (664): Not Paid
